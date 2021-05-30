1e3b89222764070243d133c62dabe7eca5da468a
pfreiman <83138702+pfreiman@users.noreply.github.com>
2021-05-30 18:06:28 -0500

    Add files via upload

---
+++ Criteria_list_constants.py
@@ -0,0 +1,3 @@
+cl_CHADS = ['CHF', 'HTN', 'AGE', 'Diabetes', 'Stroke_hx']
+cl_CHADS_VASc = ['CHF', 'HTN', 'AGE > 75', 'Diabetes', 'Stroke_hx', 'Vascular_dz', 'AGE > 65', 'Female_gender']
+cl_post_arrest = ['Unwitnessed', 'CPR_in_progress', 'ROSC_>45_minutes', 'pH<7.2']
--- Dictionary_from_checkboxes_2.py
+++ Dictionary_from_checkboxes_2.py
@@ -1,14 +1,22 @@
 from tkinter import *
+from tkinter import ttk
+
+
+import Criteria_list_constants
+from Criteria_list_constants import *
 
 root = Tk()
 
-root.title("CHADS VASc Entry Screen")
-root.geometry('800x800')
+root.title("Data Entry Screen")
+root.geometry('1000x1000')
 root.resizable(True, True)
 
-criteria_list = ['CHF', 'HTN', 'AGE', 'Diabetes', 'Stroke_hx']
+# criteria_list = ['CHF', 'HTN', 'AGE', 'Diabetes', 'Stroke_hx']  # literal list of criteria list
+criteria_list = Criteria_list_constants.cl_CHADS  # pulls in criteria list constant from constants file
+
 #  criteria_list = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo','Foxtrot']
 #  criteria_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
 print("Criteria list:", criteria_list)
 
 checkboxes = {}
@@ -20,7 +28,7 @@ result_text = ""
 
 def CreateCheckBoxes(criteria_list):
     Cbcolumn = 3
-    Cbrow = 2
+    Cbrow = 6
     Chkcount = 0
 
     for Checkbox in range(len(criteria_list)):
@@ -73,10 +81,10 @@ def show_result_in_label():
     global result_text
     labeltext = str(a)
     result_text = ("Final entries are", labeltext)
-    result_label = Label(root, text=result_text, font="helvetica, 20", bg='green', fg='yellow', height=5,
+    result_label = Label(root, text=result_text, font="helvetica, 24", bg='gray', fg='yellow',
                          relief='sunken')
     result_label.config(text=str(result_text) + "\n\n CHADS score is:  " + str(CHADS_VASc_score_dictionary_method(response_dict)))
-    result_label.grid(row=16, pady=20, column=0, columnspan=4)
+    result_label.grid(row=16, pady=50, column=1, columnspan=4)
     #  print("Result text is:", result_text)
 
 
@@ -107,24 +115,109 @@ def CHADS_VASc_score_dictionary_method(entries):
 def show_response_text():
     print("Final result text is:", result_text) # debug to see if variable is global and value is saved
 
+def open_new_window():
 
-first_label = Label(root, text="Make entries:", font="Helvetica, 20", fg="blue")
-first_label.grid(row=0, column=0)
+    global showtext
+    showtext = str(result_text) + "\n\n CHADS score is:  " + str(CHADS_VASc_score_dictionary_method(response_dict))
 
-print_cbox_button = Button(root, text='Print dictionary', height=5, width=20, fg="magenta", relief='ridge', command=CreateDictionary)
-print_cbox_button.grid(row=10, pady=20, column=0, columnspan=4)
 
-quit_button = Button(root, text="Exit", font="Helvetica, 20", fg="blue", command=quit)
-quit_button.grid(row=18, pady=20, column=0, columnspan=4)
+    new = Toplevel()
+    new.title("Results")
+    new.geometry('800x800')
+    new.resizable(True, True)
 
-result_button = Button(root, text="Print Dictionary and Show CHADS Result", font="Helvetica, 20", fg="blue", command=show_result_in_label)
-result_button.grid(row=17, pady=20, column=0, columnspan=4)
+    res_label=Label(new, text="Results appear here:  " + showtext,  bg="yellow")
+    res_label.grid(row=2, column=1, columnspan=3, pady=50)
+
+    res_close_button = Button(new, text="Close", command=new.destroy)
+    res_close_button.grid(row=4, column=3, pady=50)
+
+    res_references = Button(new, text="References")
+    res_references.grid(row=4, column=2, pady=50)
+
+    res_more_button = Button(new, text="More")
+    res_more_button.grid(row=4, column=1, pady=50)
+
+    print("Show text in new window is:", showtext)
+
+    new.mainloop()
+
+
+""" Functions for menus:"""
+
+def new_command():
+    pass
+
+def cut_command():
+    pass
+
+def copy_command():
+    pass
+
+def paste_command():
+    pass
+
+
+first_label = Label(root, text="Make entries:", font="Helvetica, 24", fg="blue")
+first_label.grid(row=2, column=0)
+
+combo_label = Label(root, text="Select topic:", fg="blue", font="Helvetica, 14")
+combo_label.grid(row=0, column=4)
+
+print_cbox_button = Button(root, text='Print dictionary', font="Helvetica, 24", fg="orange", relief='ridge', command=CreateDictionary)
+print_cbox_button.grid(row=10, pady=50, column=1, columnspan=4)
+
+result_button = Button(root, text="Print Dictionary and Show CHADS Result", font="Helvetica, 24", fg="red", command=show_result_in_label)
+result_button.grid(row=14, pady=50, column=1, columnspan=4)
+
+open_button = Button(root, text="Open new results window", font="Helvetica, 24", fg="green", command = open_new_window)
+open_button.grid(row=18, column=1, columnspan=4, pady=50)
+
+quit_button = Button(root, text="Exit", font="Helvetica, 24", fg="blue", command=quit)
+quit_button.grid(row=22, pady=50, column=4)
 
 # show_label_button = Button(root, text= "Show response text", font="Helvetica, 20", fg="blue", command=show_response_text)
 # show_label_button.grid(row=14, column=0, columnspan=4)
 
 # CHADS_button = Button(root, text="Calculate CHADS score", fg="red", font="Helvetica, 20", command=CHADS_VASc_score_dictionary_method(response_dict))
 # CHADS_button.grid(row=20, pady=20, column=0, columnspan=4)
+
+
+
+#  create a File menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_command)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+
+#  create an Edit menu
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=cut_command)
+edit_menu.add_command(label="Copy", command=copy_command)
+edit_menu.add_command(label="Paste", command=paste_command)
+
+# make a combo box that includes the criteria lists:
+
+options = [
+    "CHADS score",
+    "CHADS-VASc score",
+    "Post_code algorithm"
+]
+
+my_combo = ttk.Combobox(root, value=options)
+my_combo.current(0)
+my_combo.grid(row=0, column=5)
+
+
+
+
+
 
 
 root.mainloop()
