983817c1f70008bceb4b06d8217e0f25bfd482e9
pfreiman <pfreiman@sbcglobal.net>
2021-07-29 11:27:41 -0500

    Minor display changes

--- Working_copy.py
+++ Working_copy.py
@@ -243,9 +243,9 @@ def show_result_in_label():
     lbl_result = Label(root, text="", font="Arial, 24", bg='lemon chiffon', fg='black',
                        wraplength=500, relief='sunken')
     lbl_result["text"] = my_tools.item + " is: \n\n " + str(my_tools.result_for_current_function) + "\n"
-    lbl_result.grid(row=10, column=1, columnspan=3)
+    lbl_result.grid(row=10, column=1, columnspan=2)
 
-    btn_open_results_window.grid(row=18, column=1, columnspan=3, pady=50)
+    btn_open_results_window.grid(row=18, column=1, columnspan=2, pady=50)
 
 
 def combo_call_back_func(event):
@@ -296,20 +296,20 @@ def combo_call_back_func(event):
         lbl_intro.config(text="The corrected QT interval (QTc) is useful in evaluating the EKG "
                               "and making decisions about specific therapies.")
 
-    lbl_intro.grid(row=2, column=1, columnspan=3, pady=30)
+    lbl_intro.grid(row=2, column=1, columnspan=2, pady=30)
 
-    btn_enter_selections.grid(row=8, column=1, padx=10, pady=50, columnspan=3)
+    btn_enter_selections.grid(row=8, column=1, padx=10, pady=50, columnspan=2)
 
     """Next section chooses which frame to display"""
 
     if my_tools.item in my_tools.set_of_checkbox_questions:
-        frm_checkboxes.grid(row=4, column=1, columnspan=3, padx=20, pady=30)
+        frm_checkboxes.grid(row=4, column=1, columnspan=2, padx=20, pady=30)
         # frm_radiobuttons.grid_forget()
     if my_tools.item in my_tools.set_of_radiobutton_questions:
         # frm_checkboxes.grid_forget()
-        frm_radiobuttons.grid(row=5, column=1, columnspan=3, padx=20, pady=30)
+        frm_radiobuttons.grid(row=5, column=1, columnspan=2, padx=20, pady=30)
     if my_tools.item in my_tools.set_of_numerical_input_questions:
-        frm_numerical_entry.grid(row=6, column=1, columnspan=3, padx=20, pady=30)
+        frm_numerical_entry.grid(row=6, column=1, columnspan=2, padx=20, pady=30)
 
 
 def open_results_window():
@@ -330,14 +330,14 @@ def open_results_window():
     lbl_long_results = Label(win_results, text="", wraplength=500, justify='left')
     lbl_long_results.grid(row=5, column=1, columnspan=3)
 
-    btn_res_close = Button(win_results, text="Close", command=win_results.destroy)
-    btn_res_close.grid(row=6, column=3, pady=50)
+    btn_res_details_button = Button(win_results, text="Additional details", command=open_details_window)
+    btn_res_details_button.grid(row=6, column=1, padx=10, pady=30)
 
     btn_res_references = Button(win_results, text="References")
-    btn_res_references.grid(row=6, column=2, pady=50)
+    btn_res_references.grid(row=6, column=2, padx=10, pady=30)
 
-    btn_res_details_button = Button(win_results, text="Additional details", command=open_details_window)
-    btn_res_details_button.grid(row=6, column=1, pady=50)
+    btn_res_close = Button(win_results, text="Close", command=win_results.destroy)
+    btn_res_close.grid(row=6, column=3, padx=10, pady=30)
 
     lbl_long_results.config(text=my_tools.long_button_command())  # displays "display_long_text"  in long text label
 
@@ -384,7 +384,7 @@ lbl_combo = Label(root, text="Scores and Calculators:", fg="blue", font="Arial, 
 lbl_combo.grid(row=0, column=0, pady=10, padx=10)
 
 lbl_combo_clinical_scenarios = Label(root, text="Clinical scenarios:", fg="blue", font="Arial, 24")
-lbl_combo_clinical_scenarios.grid(row=0, column=3, pady=10, padx=10)
+lbl_combo_clinical_scenarios.grid(row=0, column=2, pady=10, padx=10)
 
 lbl_intro = Label(root, text="", font="Arial, 24", bg='lemon chiffon', fg='black',
                   wraplength=600, relief='sunken')
@@ -398,7 +398,7 @@ combo_scores_and_calculators.bind("<<ComboboxSelected>>", combo_call_back_func)
 
 combo_clinical_scenarios = ttk.Combobox(root, value=my_tools.combobox_clinical_scenarios_options_list, state="readonly")
 combo_clinical_scenarios.current(0)
-combo_clinical_scenarios.grid(row=1, column=3, padx=10, pady=10)
+combo_clinical_scenarios.grid(row=1, column=2, padx=10, pady=10)
 combo_clinical_scenarios.bind("<<ComboboxSelected>>", combo_call_back_func)
 
 btn_enter_selections = Button(root, text="Enter Selections", font="Arial, 24", fg="green", padx=20,
@@ -408,7 +408,7 @@ btn_enter_selections.grid_forget()
 
 btn_open_results_window = Button(root, text="Open Results Window", font="Arial, 24", fg="green",
                                  command=open_results_window)
-btn_open_results_window.grid(row=18, column=1, columnspan=3, pady=50)
+btn_open_results_window.grid(row=18, column=1, columnspan=2, pady=50)
 btn_open_results_window.grid_forget()
 
 btn_quit = Button(root, text="Exit", font="Arial, 24", fg="green", command=quit)
@@ -439,6 +439,4 @@ diff_dx_menu.add_command(label="Open DDx window", command=open_differential_diag
 
 root.mainloop()
 
-""" Also probably needs some way to reset the global checkboxes dictionary if this function is run more than once.
 
-"""
